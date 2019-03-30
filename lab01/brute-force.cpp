adcc1dc010368839899975b4a6a7ee8883f7d1e4
Julianny Favinha Donda <julianny.favinha@gmail.com>
2019-03-30 12:53:38 -0300

    Added instance files and calculates array of final times in M2, changed file name to brute force

--- lab01/brute-force.cpp
+++ lab01/brute-force.cpp
@@ -58,7 +58,6 @@ vector<int> read_params_file(char *name) {
     return items;
 }
 
-
 /*
 Calculates the sum of the end times of each job executed on machine 2
 */
@@ -78,25 +77,71 @@ void total_time(vector<Job> jobs, vector<int> order) {
 	cout << "Time elapsed @ machine 2: " << sum << endl;
 }
 
-void print_jobs_order(vector<int> order) {
+/*
+Calculates array of final times each job executed on machine 2
+*/
+vector<int> final_times_m2(vector<Job> jobs, vector<int> order, int k) {
+	vector<int> final_times(order.size());
+
+	int last_time_machine1 = 0;
+	int last_time_machine2 = 0;
+
+	for (int i = 0; i < order.size() && i < k; i++) {
+		int machine = order[i];
+		last_time_machine1 = last_time_machine1 + jobs[machine].time1;
+		last_time_machine2 = max(last_time_machine2, last_time_machine1) + jobs[machine].time2;
+		final_times[i] = last_time_machine2;
+	}
+
+	return final_times;
+}
+
+void print_jobs_order(vector<int> order, int k) {
 	cout << "Order: ";
-	for (int i = 0; i < order.size(); i ++) {
+	for (int i = 0; i < order.size() && i < k; i ++) {
 		cout << order[i] + 1 << " ";
 	}
 
 	cout << endl;
 }
 
+void print_vector(vector<int> v, int k) {
+	for (int i = 0; i < v.size() && i < k; i ++) {
+		cout << v[i] << " ";
+	}
+
+	cout << endl;
+}
+
+int sum_vector(vector<int> v) {
+	int sum = 0;
+	for (int i = 0; i < v.size(); i ++) {
+		sum += v[i];
+	}
+
+	return sum;
+}
+
 void brute_force(vector<Job> jobs, vector<int> order, vector<int> used, int k, int n) {
 	if (k == n) {
-		print_jobs_order(order);
-		total_time(jobs, order);
+		cout << "FOLHA ";
+		print_jobs_order(order, k);
+
+		// CALCULA SOMA DOS TEMOS FINAIS EM M2
+		cout << "TOTAL SUM: ";
+		cout << sum_vector(final_times_m2(jobs, order, k)) << endl;
 	} else {
 		for (int i = 0; i < n; i ++) {
 			if (!used[i]) {
 				used[i] = 1;
 				order[k] = i;
+
+				// print_jobs_order(order, k+1);
+				// cout << "Final times in M2: ";
+				// print_vector(final_times_m2(jobs, order, k+1), k+1);
+
 				brute_force(jobs, order, used, k+1, n);
+
 				used[i] = 0;
 			}
 		}
@@ -110,8 +155,8 @@ int main(int argc, char *argv[]) {
     vector<Job> jobs = read_jobs_file(input_file_name);
     vector<int> params = read_params_file(param_file_name);
 
-    cout << "Jobs: " << endl;
-    print_jobs(jobs);
+    // cout << "Jobs: " << endl;
+    // print_jobs(jobs);
 
     int n = jobs.size();
     vector<int> used(n, 0);
