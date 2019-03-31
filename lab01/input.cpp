d4321b95aba49bb396594e4ca24506869195314d
Julianny <julianny.donda@ifood.com.br>
2019-03-31 14:03:22 -0300

    Using the same matrix of jobs

--- lab01/input.cpp
+++ lab01/input.cpp
@@ -1,34 +1,39 @@
 #include "input.hpp"
 
-vector<Job> read_jobs_file(char *name) {
+vector< vector<int> > read_jobs_file(char *name) {
     ifstream file;
     file.open(name);
-    
+
     int n;
     file >> n;
-    
-    vector<Job> jobs(n);
-    
+
+    vector<int> machine1(n);
+    vector<int> machine2(n);
+
     int time1;
     int time2;
     int i = 0;
     while (file >> time1 >> time2) {
-        Job job = {time1, time2};
-        jobs[i] = job;
+        machine1[i] = time1;
+        machine2[i] = time2;
         i++;
     };
-    
-    file.close();
-    
+
+    vector< vector<int> > jobs;
+
+    jobs.push_back(machine1);
+    jobs.push_back(machine2);
+
     return jobs;
 }
 
-int print_jobs(vector<Job> jobs) {
-    for(int i = 0; i < jobs.size(); i++) {
-        cout << jobs[i].time1 << " " << jobs[i].time2 << endl;
+void print_jobs(vector< vector<int> > jobs) {
+    for (int i = 0; i < jobs.size(); i++) {
+        for (int j = 0; j < jobs[i].size(); j++) {
+            cout << jobs[i][j] << " ";
+        }
+        cout << endl;
     }
-    
-    return 0;
 }
 
 vector<int> read_params_file(char *name) {
@@ -45,3 +50,13 @@ vector<int> read_params_file(char *name) {
     
     return items;
 }
+
+// int main(int argc, char *argv[]) {
+//     char *input_file_name = argv[1];
+//     char *param_file_name = argv[2];
+
+//     vector< vector<int> > d = read_jobs_file(input_file_name);
+
+//     print_jobs(d);
+//     return 0;
+// }
