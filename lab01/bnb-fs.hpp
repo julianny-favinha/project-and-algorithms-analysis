8609a5a4a2c239b82e884a886ae3dfd8ca7ac971
Julianny Favinha Donda <julianny.favinha@gmail.com>
2019-04-07 15:36:59 -0300

    Add comments

--- lab01/bnb-fs.hpp
+++ lab01/bnb-fs.hpp
@@ -4,10 +4,13 @@
 #include "optimization.hpp"
 #include "input.hpp"
 
+// encontra os nos com melhores estimativas
 vector<int> bound(vector< vector<int> > jobs, int n, vector<int> order, vector<int> remaining);
 
+// recursao para encontrar a melhor solucao
 void branch(vector< vector<int> > jobs, int n, vector<int> order, vector<int> remaining);
 
+// imprime a melhor solucao encontrada
 void print_best_solution();
 
 #endif /* bnb_fs_hpp */
