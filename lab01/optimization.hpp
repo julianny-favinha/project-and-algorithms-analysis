98e1c29e25718f535c3c146724aa1cc04d801429
Julianny <julianny.donda@ifood.com.br>
2019-04-07 15:23:04 -0300

    Minor fixes

--- lab01/optimization.hpp
+++ lab01/optimization.hpp
@@ -16,16 +16,22 @@ vector<int> final_times_m1(vector< vector<int> > &jobs, vector<int> order);
 // retorna os tempos finais na maquina 2
 vector<int> final_times_m2(vector< vector<int> > &jobs, vector<int> order);
 
+// compara dois jobs na estrutura JobsMachine de acordo com o tempo na maquina 1
+bool compare_two_jobs_machine1(JobsMachine lhs, JobsMachine rhs);
+
+// compara dois jobs na estrutura JobsMachine de acordo com o tempo na maquina 2
+bool compare_two_jobs_machine2(JobsMachine lhs, JobsMachine rhs);
+
 // atualiza o limitante primal
 void update_upper_bound(vector< vector<int> > &jobs, int n, vector<int> order, vector<JobsMachine> jobs_machine);
 
 // calcula o valor para um candidato para limitante dual relaxando o problema em relacao a maquina 1
 int s1(vector< vector<int> > &jobs, int n, vector<JobsMachine> jobs_machine, vector<int> order, vector<int> remaining, vector<int> final_times_machine1);
 
-// Calcula o valor para um candidato para limitante dual relaxando o problema em relacao a maquina 2
+// calcula o valor para um candidato para limitante dual relaxando o problema em relacao a maquina 2
 int s2(vector< vector<int> > &jobs, int n, vector<JobsMachine> jobs_machine, vector<int> order, vector<int> remaining, vector<int> final_times_machine1, vector<int> final_times_machine2);
 
-vector<JobsMachine> monta_estrutura(vector< vector<int> > &jobs, vector<int> &remaining);
+vector<JobsMachine> build_jobs_machine(vector< vector<int> > &jobs, vector<int> &remaining);
 
 // calcula estimativas
 Bounds estimate_bounds(vector< vector<int> > &jobs, int n, vector<int> order, vector<int> remaining);
