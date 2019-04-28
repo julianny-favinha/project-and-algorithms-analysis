ced02d17f6cdff076f3a9f6b6e6c38334945c3f0
Julianny Favinha Donda <julianny.favinha@gmail.com>
2019-04-28 13:59:22 -0300

    Solving matching problem

--- lab02/gt10/gt10.jl
+++ lab02/gt10/gt10.jl
@@ -8,7 +8,6 @@ end
 
 read_file(ARGS)
 file_name = "gt10."*ARGS[1]*".instance"
-println(file_name)
 
 f = open(file_name, "r")
 
@@ -25,7 +24,6 @@ head = zeros(Int64, (1,1))
 tail=zeros(Int64,(1,1))
 
 while !eof(f)
-	println(">>> WHILE: lines=$lines edges=$edges")
 	line = readline(f)
 
 	if lines == 1
@@ -41,9 +39,72 @@ while !eof(f)
 	lines += 1
 end
 
-println("n=$n")
-println("m=$m")
-println("head=$head")
-println("tail=$tail")
+close(f)
+
+Edges = Array{Tuple{Int64, Int64}}(undef,m)
+for i in 1:m
+  Edges[i] = (head[i], tail[i])
+end
+
+# model
+
+time_limit = parse(Int64, ARGS[2])
+GT = Model(solver=GurobiSolver(TimeLimit=time_limit))
+
+# variables
+
+@variable(GT, x[i in Edges], Bin)
+
+# object function
+
+@objective(GT, Max, sum(x[i] for i in Edges))
+
+# constraints
+
+Ivertices=1:n
+
+for v in Ivertices
+	flag = false
+	sum = 0
+	for e in Edges
+		if v == e[1] || v == e[2]
+			sum += x[e]
+			flag = true
+		end
+	end
+
+	if flag
+		@constraint(GT, sum <= 1)
+	end
+end
+
+status = solve(GT)
+obj = getobjectivevalue(GT)
+optimal_x = getvalue(x)
+
+# relatorio 
+
+println("===============================")
+if status == :Optimal
+	println("Solução ótima encontrada.")
+elseif status == :Unbounded
+	println("Problema é ilimitado.")
+elseif status == :Infeasible
+	println("Problema é inviável.")
+elseif status == :UserLimit
+	println("Parado por limite de tempo ou iterações.")
+elseif status == :Error
+	println("Erro do resolvedor.")
+else
+	println("Não resolvido.")
+end
+	println("Número de nós explorados: ", getnodecount(GT::Model))
+
+D = getobjbound(GT::Model)
+P = getobjectivevalue(GT::Model)
+@printf("Melhor limitante dual: %.2f\n", D)
+@printf("Melhor limitante primal: %.2f\n", P)
+@printf("Gap de otimalidade: %.2f\n", (abs( D - P )/P)*100)
+@printf("Tempo de execução: %.2f\n", getsolvetime(GT::Model))
 
 end
